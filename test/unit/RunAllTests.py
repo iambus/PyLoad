bbc70a645ae553709b6418817a846b6e667eb06b
Boyu Guo <iambus@gmail.com>
2008-09-21 17:12:51 +0000

    Run all tests.

---
+++ test/unit/RunAllTests.py
@@ -0,0 +1,34 @@
+
+import os
+import glob
+
+import unittest
+
+def warning():
+	marked = ['__init__.py', 'RunAllTests.py', 'LoadTestEnv.py']
+	goodpatterns = ['Test*.py', '*.pyc', '*.log']
+	files = [i for j in map(glob.glob, goodpatterns) for i in j] + marked
+	files = set(files)
+	allfiles = set(glob.glob('*'))
+	x = allfiles.difference(files)
+	if x:
+		print 'WARNING: Ignored items:', list(x)
+		print
+
+
+def buildTestSuite():
+	suite = unittest.TestSuite()
+	for filename in glob.glob('Test*.py'):
+		modulename = os.path.splitext(filename)[0]
+		modulesuite = unittest.defaultTestLoader.loadTestsFromName(modulename)
+		suite.addTest(modulesuite)
+	return suite
+
+def main():
+	warning()
+	results = unittest.TextTestRunner().run(buildTestSuite())
+	assert results.wasSuccessful(), 'Unit tests fail!'
+
+if __name__ == '__main__':
+	main()
+
--- test/unit/__init__.py
+++ test/unit/__init__.py
@@ -1,3 +1,18 @@
 
-# TODO: run all tests
+import os
+
+def main():
+	import RunAllTests
+	RunAllTests.main()
+
+if os.path.isdir('unit'):
+	os.chdir('unit')
+	try:
+		main()
+	finally:
+		os.chdir('..')
+elif os.path.split(os.path.abspath('.'))[1] == 'unit':
+	main()
+else:
+	raise 'You are running case in a wrong directory'
 
