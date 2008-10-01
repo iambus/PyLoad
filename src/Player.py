2533d35edfd185f7a7823c19d28efbdf70bd04cb
Boyu Guo <iambus@gmail.com>
2008-10-01 03:23:18 +0000

    Remove Evalable. It looks like a unnecessary layer.

--- src/Player.py
+++ src/Player.py
@@ -1,15 +1,51 @@
 
 import Playable
-import Evalable
+import Scope
 
-class Player(Playable.Playable, Evalable.Scoped):
+class Script():
+	def __init__(self, script = '', scope = None):
+		self.script = script
+		self.scope = scope
+
+	def execute(self, scope = None):
+		if scope == None:
+			if self.scope == None:
+				self.scope = Scope.Scope()
+			scope = self.scope
+		scope.execute(self.script)
+
+	def eval(self):
+		raise NotImplementedError()
+
+	def play(self, scope):
+		self.execute(scope)
+
+class Player(Playable.Playable):
 	def __init__(self):
 		Playable.Playable.__init__(self)
-		Evalable.Scoped.__init__(self)
 
-		self.beforescript = Evalable.Script('')
-		self.afterscript = Evalable.Script('')
+		self.scope = Scope.Scope()
+		self.scripts = []
+		self.childern = []
 
+		self.beforescript = Script('')
+		self.afterscript = Script('')
+
+	def add_script(self, script):
+		self.scripts.append(script)
+	
+	def add_childern(self, child):
+		childern.append(child)
+	
+	def execute_script(self, script, base = None):
+		if base == None:
+			base = self.scope
+		script.execute(base)
+
+	def execute_child(self, child, base = None):
+		if base == None:
+			base = self.scope
+		child.play(Scope.Scope(base))
 
 	def play(self, basescope = None):
 		if basescope == None:
@@ -21,11 +57,11 @@ class Player(Playable.Playable, Evalable.Scoped):
 	def before(self, basescope = None):
 		self.execute_script(self.beforescript, basescope)
 
-	def execute(self, basescope = None):
-		self.play(basescope)
-
 	def playmain(self, basescope):
-		Evalable.Scoped.execute(self, basescope)
+		for script in self.scripts:
+			script.execute(basescope)
+		for child in self.childern:
+			child.play(Scope.Scope(basescope))
 
 	def after(self, basescope = None):
 		self.execute_script(self.afterscript, basescope)
@@ -37,7 +73,17 @@ class Controller:
 
 if __name__ == '__main__':
 	player = Player()
-	player.beforescript = Evalable.Script('print "before"')
-	player.afterscript = Evalable.Script('print "after"')
+	player.beforescript = Script('print "before"')
+	player.afterscript = Script('print "after"')
 	player.play()
+
+	scoped = Player()
+	script1 = Script('print 1')
+	script2 = Script('i = 77')
+	script3 = Script('print i')
+	scoped.add_script(script1)
+	scoped.add_script(script2)
+	scoped.add_script(script3)
+	scoped.play()
+
 
--- test/unit/TestPlayer.py
+++ test/unit/TestPlayer.py
@@ -5,6 +5,134 @@ import LoadTestEnv
 from Player import *
 from Scope import *
 
+##################################################
+# test old Evalable class Script (now in Player)
+class TestScript(unittest.TestCase):
+
+	def setUp(self):
+		self.script = Script()
+
+	def testBasic(self):
+		self.script.execute()
+
+		self.script.script = 'i = 2'
+		self.script.execute()
+
+		# i is local variable in script
+		self.script.script = 'j = i'
+		self.script.execute()
+
+		self.script.script = 'i = k'
+		self.assertRaises(NameError, self.script.execute)
+
+		scope = Scope()
+		scope['k'] = 3
+		self.script.script = 'i = k; assert i == 3'
+		self.script.execute(scope)
+
+		self.script.script = 'j = i'
+		self.script.execute()
+
+		scope = Scope()
+		self.script.script = 'j = i'
+		self.assertRaises(NameError, lambda:self.script.execute(scope))
+
+	def testConstructor(self):
+		script = Script('i = 2')
+		script.execute()
+		script.script = 'j = i'
+		script.execute()
+
+		script = Script('i = k')
+		self.assertRaises(NameError, script.execute)
+
+##################################################
+
+class B(Player):
+	def __init__(self):
+		Player.__init__(self)
+		self.script = Script('')
+
+	def play(self, base):
+		self.script.execute(base)
+
+class D(Player):
+	def __init__(self):
+		Player.__init__(self)
+		self.childern = [B(), B(), B()]
+		self.script = Script('')
+	
+	def play(self, base = None):
+		if base == None:
+			base = self.scope
+		self.script.execute(base)
+		Player.play(self, base)
+
+
+# test old Evalable class Scoped (merged into Player)
+class TestScoped(unittest.TestCase):
+
+	def setUp(self):
+		self.scoped = D()
+
+	def testBasic(self):
+		self.scoped.script.script = 'i = 2'
+		self.scoped.childern[0].script.script = 'j = i'
+		self.scoped.childern[1].script.script = 'j = i'
+		self.scoped.play()
+
+		self.scoped.childern[2].script.script = 'k = kk'
+		self.assertRaises(NameError, self.scoped.play)
+
+		self.scoped.script.script = 'x = i'
+		self.assertRaises(NameError, self.scoped.play)
+
+		self.scoped.script.script = 'x = j'
+		self.assertRaises(NameError, self.scoped.play)
+
+	def testScope(self):
+		scope = Scope()
+		scope['x'] = 9
+		self.scoped.script.script = 'i = x'
+		self.scoped.play(scope)
+		self.assertEqual(scope['i'], 9)
+		self.assertEqual(self.scoped.scope['i'], None)
+
+	def testExecuteScript(self):
+		self.scoped.script.script = 'i = 2'
+		self.scoped.play()
+
+		script = Script('j = i')
+		self.scoped.execute_script(script)
+
+		script = Script('j = k')
+		self.assertRaises(NameError, lambda:self.scoped.execute_script(script))
+
+		scope = Scope()
+		scope['k'] = 3
+		self.scoped.execute_script(script, scope)
+		self.assertEqual(scope['j'], 3)
+
+	def testExecuteChild(self):
+		self.scoped.script.script = 'i = 2'
+		script = Script('j = 3')
+		self.scoped.execute_child(script)
+		self.assertEquals(self.scoped.scope.get_names(), [])
+
+		script = Script('j = k')
+		self.assertRaises(NameError, lambda:self.scoped.execute_script(script))
+
+		scope = Scope()
+		scope['x'] = 11
+		self.scoped.execute_child(Script('j = x; x = 22'), scope)
+		self.assertEquals(self.scoped.scope.get_names(), [])
+		self.assertEquals(scope.get_names(), ['x'])
+		self.assertEquals(scope['x'], 22)
+
+		self.assertRaises(NameError, lambda:self.scoped.execute_child(Script('j = k'), scope))
+
+##################################################
+
 class H(Player):
 	def __init__(self):
 		Player.__init__(self)
@@ -83,6 +211,7 @@ class TestPlayer(unittest.TestCase):
 		self.player.p.h2.afterscript.script = 'h2 = k'
 		self.assertRaises(NameError, self.player.play)
 
+##################################################
 
 if __name__ == '__main__':
 	unittest.main()
