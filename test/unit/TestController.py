82d675c530f7d3ca50a57eca4e4bfd9e76575c48
Boyu Guo <iambus@gmail.com>
2008-10-02 05:40:17 +0000

    Add unit tests for Controller.If

--- test/unit/TestController.py
+++ test/unit/TestController.py
@@ -4,6 +4,32 @@ import LoadTestEnv
 
 from Controller import *
 
+class TestIf(unittest.TestCase):
+	def setUp(self):
+		self.If = If('i == 5')
+		self.If.beforescript = Script('i = 5')
+		self.If.add_child(Script('i = 2'))
+		self.If.afterscript = Script('assert i == 2')
+	
+	def testBasic(self):
+		self.If.play()
+	
+	def testChildScope(self):
+		self.If = If('True')
+		self.If.add_child(Script('i = 2'))
+		self.If.afterscript = Script('assert i == 2')
+		self.assertRaises(NameError, self.If.play)
+
+	def testScriptScope(self):
+		self.If = If('True')
+		self.If.add_script(Script('i = 2'))
+		self.If.afterscript = Script('assert i == 2')
+		self.If.play()
+
+	def testException(self):
+		self.If = If('x == 5')
+		self.If.play()
+
 class TestLoop(unittest.TestCase):
 	def setUp(self):
 		self.loop = Loop('x < 10')
@@ -17,6 +43,14 @@ class TestLoop(unittest.TestCase):
 
 		self.assertEqual(self.loop.scope.lookup('x'), 10)
 		self.assertEqual(self.loop.scope.lookup('y'), 10)
+	
+	def testChild(self):
+		self.loop = Loop('x < 10')
+		self.loop.beforescript = Script('x = 0;y = 0')
+		self.loop.add_child(Script('x += 1'))
+		self.loop.add_child(Script('y += 1'))
+		self.loop.afterscript = Script('assert x >= 10; assert y == 10')
+		self.loop.play()
 
 	def testBreak(self):
 		import Globals
