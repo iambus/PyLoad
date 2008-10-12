166981582a36925d0d668d7c04b15ea369188d1f
Boyu Guo <iambus@gmail.com>
2008-10-12 16:14:22 +0000

    Add some useless code for UnicodeCoder.

--- src/Coder.py
+++ src/Coder.py
@@ -24,12 +24,38 @@ class Rot13Coder:
 		raise NotImplementedError()
 
 class UnicodeCoder:
+	def __init__(self):
+		self.encoding = 'ascii'
+
 	@classmethod
 	def encode(cls, s):
-		raise NotImplementedError()
+		if type(s) == str:
+			encodings = ['ascii', 'utf-8', 'gbk']
+			for encoding in encodings:
+				try:
+					s = s.decode(encoding)
+				except UnicodeDecodeError:
+					continue
+				else:
+					break
+		return s
+
 	@classmethod
 	def decode(cls, s):
-		raise NotImplementedError()
+		if type(s) == unicode:
+			encodings = ['ascii', 'utf-8', 'gbk']
+			for encoding in encodings:
+				try:
+					s = s.encode(encoding)
+				except UnicodeEncodeError:
+					continue
+				else:
+					break
+		return s
+
+# TODO
+class GZipCoder:
+	pass
 
 from amf.Coder import SimpleAMFCoder as AMFCoder
 
