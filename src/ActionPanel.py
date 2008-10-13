0ddb915bd15968285bfacb7bc4a047cc841af84b
Boyu Guo <iambus@gmail.com>
2008-10-13 14:07:38 +0000

    Delete a useless module.

--- src/ActionPanel.py
+++
@@ -1,8 +0,0 @@
-
-import wx
-
-class ActionPanel(wx.Window):
-	def __init__(self, parent, color = 'red'):
-		wx.Window.__init__(self, parent, -1, style = wx.SIMPLE_BORDER)
-		self.SetBackgroundColour(color)
-
