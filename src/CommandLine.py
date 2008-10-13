3eb3a5969b9923171123b34fe6c69bb1e3de768a
Boyu Guo <iambus@gmail.com>
2008-10-13 13:52:54 +0000

    Introduce a module CommandLine. Not implemented.

---
+++ src/CommandLine.py
@@ -0,0 +1,18 @@
+
+raise NotImplementedError()
+
+def load_project(path):
+	raise NotImplementedError()
+
+def save_project(project, path):
+	raise NotImplementedError()
+
+def play_project(project):
+	raise NotImplementedError()
+
+def read_report(path):
+	raise NotImplementedError()
+
+if __name__ == '__main__':
+	raise NotImplementedError()
+
