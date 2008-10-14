b383df1a8ea3b0af77a1f05f6c88923ff2e40fbf
Boyu Guo <iambus@gmail.com>
2008-10-14 07:53:58 +0000

    Support playing from CommandLine.

--- src/CommandLine.py
+++ src/CommandLine.py
@@ -9,8 +9,56 @@ def load_project(path):
 def save_project(project, path):
 	project.save(path)
 
-def play_project(project):
-	raise NotImplementedError()
+#FIXME: dirty and duplicated code
+def play_project(project, reporter = None):
+	assert isinstance(project, Project)
+
+	if project.current_special == None:
+		raise RuntimeError('No special to start!')
+
+	user_count = project.user_count
+	iteration_count = project.iteration_count
+	special = project.current_special
+	user_factory = project.user_factory
+	iteration_factory = project.iteration_factory
+	global_factory = project.global_factory
+
+	from Report import Report
+	if type(reporter) in (str, unicode):
+		reporter = Report(reporter)
+	if reporter != None:
+		assert isinstance(reporter, Report)
+
+		import datetime
+		start_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		summary = 'Start Time: %s\nUser Count: %s\nIteration Count: %s\nSpecial: %s' % (start_time, user_count, iteration_count, special.label)
+
+		import ReportManager
+		ReportManager.start_report(reporter = reporter, project = project, summary = summary)
+
+	import PlayPolicy
+	policy = PlayPolicy.IterationBasedPlayPolicy(
+			player = special,
+			user_count = user_count,
+			iteration_count = iteration_count,
+			user_factory = user_factory,
+			iteration_factory = iteration_factory,
+			global_factory = global_factory,
+			reporter = reporter
+			)
+
+	import sys
+	sys.path.append('runtime')
+	sys.path.append('plugin')
+
+	policy.play()
+
+	if len(sys.path) >= 2 and sys.path[-2:] == ['runtime', 'plugin']:
+		sys.path.pop()
+		sys.path.pop()
+
+	if reporter:
+		reporter.finish()
 
 def clone_project(project):
 	data = project.repository_internal
@@ -46,7 +94,9 @@ def test():
 #	import Repository
 #	Repository.trace_classes()
 
-	clean_project_in_path('a.pkl')
+	project = load_project('workflow.pkl')
+	project.user_count = 3
+	play_project(project, 'b-report.db')
 
 if __name__ == '__main__':
 	#main()
