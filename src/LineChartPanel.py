231da1ef87d9d50f67272973a28a313c9e47a596
Boyu Guo <iambus@gmail.com>
2008-10-18 18:18:27 +0000

    Add a simple line chart for report.

---
+++ src/LineChartPanel.py
@@ -0,0 +1,207 @@
+
+import wx
+
+def XTimeFormatter(s, max = None):
+	if max and max < 60:
+		return '%ds' % s
+	elif max and max < 3600:
+		return '%02dm:%02ds' % ((s/60)%60, s%60)
+	return '%02d:%02d:%02d' % (s/3600, (s/60)%60, s%60)
+
+def YMSFormatter(ms, max = None):
+	if (max and max >= 1000) or ms >= 1000:
+		s = ms / 1000.0
+		return '%ss' % s
+	else:
+		return '%sms' % ms
+
+def DefaultFormatter(x, max = None):
+	return str(x)
+
+class LineChart(wx.Panel): 
+	def __init__(self, parent, times = None):
+		wx.Panel.__init__(self, parent)
+		self.SetBackgroundColour('WHITE')
+
+		self.times = times
+
+		self.xformat = DefaultFormatter
+		self.yformat = DefaultFormatter
+
+		self.InitData()
+		self.InitBuffer()
+
+		self.Bind(wx.EVT_PAINT, self.OnPaint)
+		self.Bind(wx.EVT_SIZE, self.OnSize)
+		self.Bind(wx.EVT_IDLE, self.OnIdle)
+
+	def InitData(self):
+		if self.times == None:
+			return
+
+		from itertools import groupby
+		from operator import itemgetter
+		self.data = sorted([(i[0]/1000+1, i[1]) for i in self.times])
+		self.groups = [(k, zip(*g)[1]) for k, g in groupby(self.data, key=itemgetter(0))]
+		self.max = [(k, max(v)) for k, v in self.groups]
+		self.min = [(k, min(v)) for k, v in self.groups]
+		self.avg = [(k, sum(v)/len(v)) for k, v in self.groups]
+
+		self.xmax = max(zip(*self.data)[0])
+		self.ymax = max(zip(*self.data)[1])
+
+		if self.xmax < 30:
+			self.xi = 1
+		elif self.xmax < 60 * 10:
+			self.xi = 30
+		else:
+			self.xi = 60 * 2
+
+		self.xmax = (self.xmax / self.xi + 1) * self.xi
+
+		if self.ymax < 1000:
+			self.yi = 100
+		elif self.ymax < 1000 * 2:
+			self.yi = 200
+		elif self.ymax < 1000 * 5:
+			self.yi = 500
+		elif self.ymax < 1000 * 10:
+			self.yi = 1000
+		elif self.ymax < 1000 * 60:
+			self.yi = 5000
+		elif self.ymax < 1000 * 120:
+			self.yi = 1000 * 10
+		else:
+			self.yi = 1000 * 30
+
+		self.ymax = (self.ymax / self.yi + 1) * self.yi
+
+	def InitBuffer(self):
+		size = self.GetClientSize()
+		self.buffer = wx.EmptyBitmap(size.width, size.height)
+		dc = wx.BufferedDC(None, self.buffer)
+		dc.SetBackground(wx.Brush(self.GetBackgroundColour()))
+		dc.Clear()
+
+		self.DrawAll(dc)
+		self.reInitBuffer = False
+
+	# Event handlers
+	def OnSize(self, event):
+		self.reInitBuffer = True
+
+	def OnIdle(self, event):
+		if self.reInitBuffer:
+			self.InitBuffer()
+			self.Refresh(False)
+
+	def OnPaint(self, event):
+		dc = wx.BufferedPaintDC(self, self.buffer)
+
+	# Drawing
+	def DrawAll(self, dc):
+		if self.times == None:
+			return
+
+		w, h = dc.GetSize()
+		dc.SetDeviceOrigin(30, h-30)
+		dc.SetAxisOrientation(True, True)
+		dc.SetPen(wx.Pen('WHITE'))
+		dc.DrawRectangle(1, 1, w, h)
+
+		self.DrawGrid(dc)
+		self.DrawAxis(dc)
+		#self.DrawTitle(dc)
+		self.DrawData(dc)
+
+	def DrawAxis(self, dc):
+		w, h = dc.GetSize()
+		w, h = w - 40, h - 30
+		if w <= 0 or h <= 0:
+			return
+
+		dc.SetPen(wx.Pen('#0AB1FF'))
+		font = dc.GetFont()
+		font.SetPointSize(8)
+		dc.SetFont(font)
+		dc.DrawLine(0, 0, w+40, 0)
+		dc.DrawLine(0, 0, 0, h+30)
+
+		for i in range(self.xi, self.xmax+1, self.xi):
+			x = w * i / self.xmax
+			dc.DrawLine(x, 1, x, -5)
+			dc.DrawText(self.xformat(i, self.xmax), x-13, -10)
+
+		for i in range(self.yi, self.ymax+1, self.yi):
+			y = h * i / self.ymax
+			dc.DrawLine(1, y, -5, y)
+			#dc.DrawText(str(i), -30, y+5)
+			dc.DrawText(self.yformat(i, self.ymax), -30, y)
+
+	def DrawGrid(self, dc):
+		w, h = dc.GetSize()
+		w, h = w - 40, h - 30
+		dc.SetPen(wx.Pen('#d5d5d5'))
+
+		for i in range(self.xi, self.xmax+1, self.xi):
+			x = w * i / self.xmax
+			dc.DrawLine(x, 0, x, h)
+
+		for i in range(self.yi, self.ymax+1, self.yi):
+			y = h * i / self.ymax
+			dc.DrawLine(0, y, w, y)
+
+	def DrawTitle(self, dc):
+		w, h = dc.GetSize()
+		w, h = w - 40, h - 30
+		font =  dc.GetFont()
+		font.SetWeight(wx.FONTWEIGHT_BOLD)
+		dc.SetFont(font)
+		dc.DrawText('Some Text...', 90, 235)
+
+
+	def DrawData(self, dc):
+		dc.SetPen(wx.Pen('RED'))
+		self.DrawLine(dc, self.max)
+
+		dc.SetPen(wx.Pen('YELLOW'))
+		self.DrawLine(dc, self.min)
+
+		dc.SetPen(wx.Pen('#0ab1ff'))
+		self.DrawLine(dc, self.avg)
+
+
+	def DrawLine(self, dc, lineData):
+		w, h = dc.GetSize()
+		w, h = w - 40, h - 30
+
+		points = [(0,0)]
+		points.extend(lineData)
+		points.append(points[-1])
+
+		xv = float(w) / self.xmax
+		yv = float(h) / self.ymax
+
+		dc.DrawSpline([(x*xv, y*yv) for x, y in points])
+
+	def SetData(self, times):
+		self.times = times
+		self.InitData()
+		self.reInitBuffer = True
+
+
+if __name__ == '__main__':
+	times = ((116, 173), 
+			(121, 216), 
+			(1401, 208), 
+			(1508, 225), 
+			(1409, 214), 
+			(1700, 228), 
+			(1850, 238), 
+			(1960, 272), 
+			(2600, 376), 
+			(1890, 274), )
+
+	import Test
+	Test.TestPanel(LineChart, lambda p: p.SetData(times))
+
---
+++ src/ReportTab.py
@@ -0,0 +1,97 @@
+
+import wx
+import LineChartPanel
+
+class ReportTab(wx.Panel):
+	def __init__(self, parent):
+		wx.Panel.__init__(self, parent, -1)
+
+		self.splitter = wx.SplitterWindow(self, style=wx.BORDER_NONE)
+
+		self.list = wx.ListCtrl(self.splitter, -1,
+				style=wx.LC_REPORT
+				#| wx.BORDER_SUNKEN
+				#| wx.BORDER_NONE
+				#| wx.LC_EDIT_LABELS
+				#| wx.LC_SORT_ASCENDING
+				#| wx.LC_NO_HEADER
+				#| wx.LC_VRULES
+				#| wx.LC_HRULES
+				| wx.LC_SINGLE_SEL
+				)
+		self.list.InsertColumn(0, "ID")
+		self.list.InsertColumn(1, "Label")
+		self.list.InsertColumn(2, "Avg")
+		self.list.InsertColumn(3, "Max")
+		self.list.InsertColumn(4, "Min")
+		self.list.InsertColumn(5, "Count")
+
+		self.list.SetColumnWidth(0, 60)
+		self.list.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+		self.list.SetColumnWidth(2, 60)
+		self.list.SetColumnWidth(3, 60)
+		self.list.SetColumnWidth(4, 60)
+		self.list.SetColumnWidth(5, 60)
+
+
+		self.chartPanel = LineChartPanel.LineChart(self.splitter)
+		self.chartPanel.xformat = LineChartPanel.XTimeFormatter
+		self.chartPanel.yformat = LineChartPanel.YMSFormatter
+
+		self.splitter.SplitHorizontally(self.list, self.chartPanel, 180)
+
+		import Layout
+		Layout.SingleLayout(self, self.splitter)
+
+		self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnItemSelected, self.list)
+
+	def LoadReport(self, path):
+		import sqlite3
+
+		connection = sqlite3.connect(path)
+		cursor = connection.cursor()
+
+		cursor.execute('select id, label, avg, max, min, count from summary')
+		self.summary = [row for row in cursor]
+		self.LoadSummary(self.summary)
+
+		cursor.execute('select hitid, timestamp, response_time from hits_v order by hitid, timestamp')
+		self.data = [row for row in cursor]
+
+		cursor.close()
+
+	def LoadSummary(self, rows):
+		self.list.DeleteAllItems()
+		def tostr(v):
+			if type(v) in [str, unicode]:
+				return v
+			elif type(v) in [float]:
+				return '%.3f' % v
+			else:
+				return str(v)
+
+		for i in range(len(rows)):
+			row = rows[i]
+			row = map(tostr, row)
+			index = self.list.InsertStringItem(i, row[0])
+			self.list.SetStringItem(index, 1, row[1])
+			self.list.SetStringItem(index, 2, row[2])
+			self.list.SetStringItem(index, 3, row[3])
+			self.list.SetStringItem(index, 4, row[4])
+			self.list.SetStringItem(index, 5, row[5])
+
+		self.list.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+
+	def LoadChart(self, uid):
+		data = [hit[1:3] for hit in self.data if hit[0] == uid]
+		self.chartPanel.SetData(data)
+
+
+	def OnItemSelected(self, event):
+		uid = self.list.GetItemText(event.m_itemIndex)
+		self.LoadChart(uid)
+
+if __name__ == '__main__':
+	import Test
+	Test.TestPanel(ReportTab, lambda p:p.LoadReport('reports/last-report.db'))
+
