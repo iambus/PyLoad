d383659da588cef0e8d66774fdacf777011af982
Boyu Guo <iambus@gmail.com>
2008-10-20 13:29:43 +0000

    use Template.substitute instead of Template.safe_substitute, as "safe" can hide some errors.

--- src/Template.py
+++ src/Template.py
@@ -5,7 +5,7 @@ def escape(s):
 
 def subst(template, variables):
 	from string import Template
-	return Template(template).safe_substitute(variables)
+	return Template(template).substitute(variables)
 
 def eval(template, variables):
 	raise NotImplementedError('eval is not implemented')
