0a1337460b6da8c50f1c606e361cbb27e68140ff
Boyu Guo <iambus@gmail.com>
2008-10-21 09:29:47 +0000

    time.sleep is very Useful...

--- src/runtime/Useful.py
+++ src/runtime/Useful.py
@@ -1,7 +1,7 @@
 
 import cookielib, urllib2
 
-__all__ = ['Cookie', 'Browser']
+__all__ = ['Cookie', 'Browser', 'sleep']
 
 def Cookie():
 	return cookielib.CookieJar()
@@ -10,4 +10,5 @@ def Browser():
 	cj = cookielib.CookieJar()
 	return urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
+from time import sleep
 
