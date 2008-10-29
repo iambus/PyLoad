640a731b7e9d52e9f34cda2eeeb3723be78e96d1
Boyu Guo <iambus@gmail.com>
2008-10-29 17:25:28 +0000

    A new (and still simple) proxy implemented in Twisted.

---
+++ src/proxy/NewTwistedProxy.py
@@ -0,0 +1,58 @@
+from twisted.web import proxy, http
+from twisted.python import log
+
+#import sys
+#log.startLogging(sys.stdout)
+log.startLogging(open('twisted-proxy.log', 'w'))
+
+PROXY_PORT = 9107
+
+class NewProxyClient(proxy.ProxyClient):
+
+	def handleHeader(self, key, value):
+		proxy.ProxyClient.handleHeader(self, key, value)
+
+	def handleResponsePart(self, data):
+		proxy.ProxyClient.handleResponsePart(self, data)
+
+	def handleResponseEnd(self):
+		proxy.ProxyClient.handleResponseEnd(self)
+
+	def connectionMade(self):
+		print 'connected'
+		proxy.ProxyClient.connectionMade(self)
+
+class NewProxyClientFactory(proxy.ProxyClientFactory):
+	def buildProtocol(self, addr):
+		client = proxy.ProxyClientFactory.buildProtocol(self, addr)
+		client.__class__ = NewProxyClient
+		return client
+
+class NewProxyRequest(proxy.ProxyRequest):
+	protocols = {'http': NewProxyClientFactory}
+
+	def __init__(self, *args):
+		proxy.ProxyRequest.__init__(self, *args)
+
+
+class NewProxy(proxy.Proxy):
+	def __init__(self):
+		proxy.Proxy.__init__(self)
+
+	def requestFactory(self, *args):
+		return NewProxyRequest(*args)
+
+class NewProxyFactory(http.HTTPFactory):
+	def __init__(self):
+		http.HTTPFactory.__init__(self)
+
+	def buildProtocol(self, addr):
+		protocol = NewProxy()
+		return protocol
+
+if __name__ == "__main__":
+	from twisted.internet import reactor
+	prox = NewProxyFactory()
+	reactor.listenTCP(PROXY_PORT, prox)
+	reactor.run( )
+
