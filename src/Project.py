33fa9e2493eee0b215adbc7d37ae3f15528204e8
Boyu Guo <iambus@gmail.com>
2008-12-25 09:02:09 +0000

    Move most project operation code from CommandLine to Project.

--- src/Project.py
+++ src/Project.py
@@ -4,6 +4,8 @@ import os
 import os.path
 import Repository
 
+##################################################
+
 class Project:
 	def __init__(self):
 		self.records = []
@@ -100,6 +102,98 @@ class NoneProject:
 		pass
 	def load(self):
 		pass
+
+##################################################
+# Help functions, mainly for CommandLine usage
+
+def load_project(path):
+	project = Project()
+	project.load(path)
+	return project
+
+def save_project(project, path):
+	project.save(path)
+
+#FIXME: dirty and duplicated code
+def play_project(project, reporter = None, project_path = 'unkown'):
+	assert isinstance(project, Project)
+
+	if project.current_special == None:
+		raise RuntimeError('No special to start!')
+
+	user_count = project.user_count
+	iteration_count = project.iteration_count
+	special = project.current_special
+	user_factory = project.user_factory
+	iteration_factory = project.iteration_factory
+	global_factory = project.global_factory
+
+	if reporter != None:
+		from Report import Report
+		if type(reporter) in (str, unicode):
+			reporter = Report(reporter)
+		assert isinstance(reporter, Report)
+
+		import datetime
+		start_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		info = {
+				'Start Time': start_time,
+				'User Count': user_count,
+				'Iteration Count': iteration_count,
+				'Special': special.label,
+				'Project Path': project_path,
+			}
+
+		import ReportManager
+		ReportManager.start_report(reporter = reporter, project = project, info = info)
+
+	import PlayPolicy
+	policy = PlayPolicy.IterationBasedPlayPolicy(
+			player = special,
+			user_count = user_count,
+			iteration_count = iteration_count,
+			user_factory = user_factory,
+			iteration_factory = iteration_factory,
+			global_factory = global_factory,
+			reporter = reporter
+			)
+
+	import sys
+	sys.path.append('runtime')
+	sys.path.append('plugin')
+
+	policy.play()
+
+	if len(sys.path) >= 2 and sys.path[-2:] == ['runtime', 'plugin']:
+		sys.path.pop()
+		sys.path.pop()
+
+	if reporter:
+		reporter.finish()
+
+def clone_project(project):
+	data = project.repository_internal
+	project.repository_internal = None
+
+	import Repository
+	new_data = Repository.RepositoryInternal()
+	import Clone
+	new_project = Clone.clone(project, Repository.LocalRepository(new_data))
+
+	project.repository_internal = data
+	new_project.repository_internal = new_data
+
+	return new_project
+
+def clean_project(project):
+	raise NotImplementedError()
+
+def clean_project_in_path(path):
+	project = load_project(path)
+	new_project = clone_project(project)
+	save_project(new_project, path)
+
+##################################################
 
 if __name__ == '__main__':
 	import pickle
