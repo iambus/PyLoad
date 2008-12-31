4920c1f8bb40f519709809703d2f3efe3e0845d8
Boyu Guo <iambus@gmail.com>
2008-12-31 07:46:07 +0000

    Small refactoring.

--- src/util/cpuinfo/__init__.py
+++ src/util/cpuinfo/__init__.py
@@ -1,12 +1,14 @@
 
 import platform
 
-if platform.uname()[0] == 'Linux':
+osname = platform.uname()[0]
+
+if osname == 'Linux':
 	from linux import *
 	__all__ = linux.__all__
-elif platform.uname()[0] == 'Windows':
+elif osname == 'Windows':
 	from win import *
 	__all__ = win.__all__
 else:
-	raise OSError('Supported platform %s' % platform.uname()[0])
+	raise OSError('Supported platform %s' % osname)
 
