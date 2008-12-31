ec65313eef8bb327c48c7ca3b4a45aecf482e832
Boyu Guo <iambus@gmail.com>
2008-12-31 05:25:21 +0000

    Put platform-dependent code into separated file.

--- src/util/cpu.py
+++ src/util/cpu.py
@@ -2,53 +2,21 @@
 import time
 import sys
 import platform
-assert platform.uname()[0] == 'Linux', 'This script must be run under Linux'
+
+if platform.uname()[0] == 'Linux':
+    from linuxcpu import read_current_cpu_point, cpu_percentage_between_points
+elif platform.uname()[0] == 'Windows':
+    raise NotImplementedError()
+    from wincpu import read_current_cpu_point, cpu_percentage_between_points
+else:
+    raise OSError('Supported platform %s' % platform.uname()[0])
 
 ##################################################
 ###################  Recording  ##################
 ##################################################
 
-#def get_cores_number():
-#    fp = open('/proc/cpuinfo')
-#    lines = fp.readlines()
-#    fp.close()
-#    for line in lines:
-#        if line.startswith('cpu core'):
-#            return int(line[line.index(':')+1:])
-#
-#cores_number = get_cores_number()
-
-def read_current_cpu_point():
-    fp = open('/proc/stat')
-    lines = fp.readlines()
-    fp.close()
-
-    line = lines[0]
-    values = line.split()[1:]
-
-    assert len(values) == 7 or len(values) == 8
-
-    user    = int(values[0])
-    nice    = int(values[1])
-    system  = int(values[2])
-    idle    = int(values[3])
-    iowait  = int(values[4])
-    irq     = int(values[5])
-    softirq = int(values[6])
-
-    return user, system, idle
-
 def read_current_time_and_cpu():
     return time.time(), read_current_cpu_point()
-
-def cpu_percentage_between_points(p1, p2):
-    user1, system1, idle1 = p1
-    user2, system2, idle2 = p2
-    u = user2 - user1
-    s = system2 - system1
-    i = idle2 - idle1
-    return (u + s) * 100.0 / (u + s + i)
-
 
 def record_loop(logname):
     fp = open(logname, 'w') if logname else None
@@ -215,3 +183,4 @@ if __name__ == '__main__':
     main()
 
 
+# vim: expandtab:shiftwidth=4
