40647d2867f79e24003a1f6bbbad290e0e5f062f
Boyu Guo <iambus@gmail.com>
2009-02-27 11:44:34 +0000

    Support set_host for Special, as I need this in one application

--- src/Special.py
+++ src/Special.py
@@ -5,3 +5,18 @@ class Special(Player.Player):
 	def __init__(self):
 		Player.Player.__init__(self)
 
+	def set_host(self, host):
+		seen = []
+		def set_your_host(node, host):
+			if node in seen:
+				return
+			seen.append(node)
+			if hasattr(node, 'set_host'):
+				node.set_host(host)
+				return
+			else:
+				for child in node.childern:
+					set_your_host(child, host)
+		for c in self.childern:
+			set_your_host(c, host)
+
