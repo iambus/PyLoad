9ce857c0c3c416b174393a701f7b062f2de380d6
Boyu Guo <iambus@gmail.com>
2009-03-24 14:12:29 +0000

    FlyFrame: playing code on the fly

---
+++ src/FlyFrame.py
@@ -0,0 +1,61 @@
+
+import wx
+import wx.lib.layoutf
+from EditorPanel import EditorPanel
+
+
+class FlyPanel(wx.Panel):
+    def __init__(self, parent, **vars):
+	wx.Panel.__init__(self, parent, -1)
+	
+	self.vars = vars
+	
+	self.editor = EditorPanel(self, -1)
+	self.testButton = wx.Button(self, -1, "Test")
+
+	# Layout
+	self.SetAutoLayout(True)
+	self.editor.SetConstraints(
+	    wx.lib.layoutf.Layoutf('t=t10#1;l=l10#1;b%b90#1;r=r10#1',(self,)))
+	self.testButton.SetConstraints(
+	    wx.lib.layoutf.Layoutf('t_10#2;l=l10#1;h*;w*',(self,self.editor)))
+
+	self.editor.saveButton.Hide()
+	self.Bind(wx.EVT_BUTTON, self.OnPlay, self.testButton)
+
+    def OnPlay(self, event):
+	script = self.editor.GetValue()
+	exec script in self.vars
+
+class FlyFrame(wx.Frame):
+    def __init__(self, parent, **vars):
+	wx.Frame.__init__(self, parent, -1, "Fly")
+	
+	panel = FlyPanel(self, **vars)
+
+	self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+
+    def OnCloseWindow(self, event):
+	self.Destroy()
+
+def fly(parent, **vars):
+    win = FlyFrame(parent, **vars)
+    win.Show(True)
+
+if __name__ == '__main__':
+
+    class TestPanel(wx.Panel):
+	def __init__(self, parent):
+	    wx.Panel.__init__(self, parent, -1)
+
+	    b = wx.Button(self, -1, "Create and Show a Frame", (50,50))
+	    self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+
+	def OnButton(self, evt):
+	    show(self)
+
+    import Test
+    Test.TestPanel(TestPanel)
+
+
+
