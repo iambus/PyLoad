5ba528aff35edb4b06bf68fc5eabc2984911aea5
Boyu Guo <iambus@gmail.com>
2009-03-26 05:32:16 +0000

    Revert stupid r1138, which committed some debug code...

--- src/amf/Coder.py
+++ src/amf/Coder.py
@@ -3,10 +3,6 @@ from AMFDecoder import AMFDecoder
 from AMFEncoder import AMFEncoder
 from AMFXML2 import ToXML, FromXML
 
-from uuid import uuid1
-def new_uuid(x):
-	return r'"messageId">%s</member>' % uuid1()
-
 class SimpleAMFCoder:
 	@classmethod
 	def encode(cls, xml):
@@ -15,8 +11,6 @@ class SimpleAMFCoder:
 		#messageId = str(uuid.uuid1())
 		#import re
 		#xml = re.sub(r'"messageId">[^<>]+</member>', r'"messageId">'+messageId+r'</member>', xml)
-		#import re
-		#xml = re.sub(r'"messageId">[^<>]+</member>', new_uuid, xml)
 
 		fromxml = FromXML(xml)
 		packet = fromxml.get_packet()
@@ -24,16 +18,8 @@ class SimpleAMFCoder:
 		return encoder.encode()
 	@classmethod
 	def decode(cls, raw):
-		try:
-			decoder = AMFDecoder(raw)
-			packet = decoder.decode()
-			toxml = ToXML(packet)
-			return toxml.get_xml()
-		except:
-			import datetime
-			filename = datetime.datetime.now().strftime('can-not-be-decoded-amf-%Y-%m-%d-%H-%M-%S.txt')
-			fp = open(filename, 'wb')
-			fp.write(raw)
-			fp.close()
-			raise
+		decoder = AMFDecoder(raw)
+		packet = decoder.decode()
+		toxml = ToXML(packet)
+		return toxml.get_xml()
 
