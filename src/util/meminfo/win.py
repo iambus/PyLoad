d9dc2543bec4ccdc987bb24c54330ceb9e668342
Boyu Guo <iambus@gmail.com>
2009-04-27 06:44:35 +0000

    Remove privilege code.

--- src/util/meminfo/win.py
+++ src/util/meminfo/win.py
@@ -9,7 +9,6 @@ from ctypes.wintypes import *
 
 ##################################################
 
-NULL = 0
 
 PROCESS_QUERY_INFORMATION = 0x0400L
 PROCESS_VM_READ           = 0x0010L
@@ -25,96 +24,6 @@ class PROCESS_MEMORY_COUNTERS(Structure):
 				('QuotaNonPagedPoolUsage', c_size_t),
 				('PagefileUsage', c_size_t),
 				('PeakPagefileUsage', c_size_t),)
-
-class LUID(Structure):
-	_fields_ = (('LowPart', DWORD),
-				('HighPart', LONG),)
-
-class LUID_AND_ATTRIBUTES(Structure):
-	_fields_ = (('Luid', LUID),
-				('Attributes', ULONG),)
-
-ANYSIZE_ARRAY = 1
-class TOKEN_PRIVILEGES(Structure):
-	_fields_ = (('PrivilegeCount', DWORD),
-				('Privileges', LUID_AND_ATTRIBUTES * ANYSIZE_ARRAY),)
-
-
-SE_PRIVILEGE_ENABLED_BY_DEFAULT = 0x00000001
-SE_PRIVILEGE_ENABLED            = 0x00000002
-SE_PRIVILEGE_USED_FOR_ACCESS    = 0x80000000
-
-SE_CREATE_TOKEN_NAME              = "SeCreateTokenPrivilege"
-SE_ASSIGNPRIMARYTOKEN_NAME        = "SeAssignPrimaryTokenPrivilege"
-SE_LOCK_MEMORY_NAME               = "SeLockMemoryPrivilege"
-SE_INCREASE_QUOTA_NAME            = "SeIncreaseQuotaPrivilege"
-SE_UNSOLICITED_INPUT_NAME         = "SeUnsolicitedInputPrivilege"
-SE_MACHINE_ACCOUNT_NAME           = "SeMachineAccountPrivilege"
-SE_TCB_NAME                       = "SeTcbPrivilege"
-SE_SECURITY_NAME                  = "SeSecurityPrivilege"
-SE_TAKE_OWNERSHIP_NAME            = "SeTakeOwnershipPrivilege"
-SE_LOAD_DRIVER_NAME               = "SeLoadDriverPrivilege"
-SE_SYSTEM_PROFILE_NAME            = "SeSystemProfilePrivilege"
-SE_SYSTEMTIME_NAME                = "SeSystemtimePrivilege"
-SE_PROF_SINGLE_PROCESS_NAME       = "SeProfileSingleProcessPrivilege"
-SE_INC_BASE_PRIORITY_NAME         = "SeIncreaseBasePriorityPrivilege"
-SE_CREATE_PAGEFILE_NAME           = "SeCreatePagefilePrivilege"
-SE_CREATE_PERMANENT_NAME          = "SeCreatePermanentPrivilege"
-SE_BACKUP_NAME                    = "SeBackupPrivilege"
-SE_RESTORE_NAME                   = "SeRestorePrivilege"
-SE_SHUTDOWN_NAME                  = "SeShutdownPrivilege"
-SE_DEBUG_NAME                     = "SeDebugPrivilege"
-SE_AUDIT_NAME                     = "SeAuditPrivilege"
-SE_SYSTEM_ENVIRONMENT_NAME        = "SeSystemEnvironmentPrivilege"
-SE_CHANGE_NOTIFY_NAME             = "SeChangeNotifyPrivilege"
-SE_REMOTE_SHUTDOWN_NAME           = "SeRemoteShutdownPrivilege"
-SE_UNDOCK_NAME                    = "SeUndockPrivilege"
-SE_SYNC_AGENT_NAME                = "SeSyncAgentPrivilege"
-SE_ENABLE_DELEGATION_NAME         = "SeEnableDelegationPrivilege"
-SE_MANAGE_VOLUME_NAME             = "SeManageVolumePrivilege"
-SE_IMPERSONATE_NAME               = "SeImpersonatePrivilege"
-SE_CREATE_GLOBAL_NAME             = "SeCreateGlobalPrivilege"
-
-
-TOKEN_ASSIGN_PRIMARY    = 0x0001
-TOKEN_DUPLICATE         = 0x0002
-TOKEN_IMPERSONATE       = 0x0004
-TOKEN_QUERY             = 0x0008
-TOKEN_QUERY_SOURCE      = 0x0010
-TOKEN_ADJUST_PRIVILEGES = 0x0020
-TOKEN_ADJUST_GROUPS     = 0x0040
-TOKEN_ADJUST_DEFAULT    = 0x0080
-TOKEN_ADJUST_SESSIONID  = 0x0100
-
-def EnablePriv(lpszPriv):
-	hToken = HANDLE()
-	luid = LUID()
-	tkprivs = TOKEN_PRIVILEGES()
-
-#	windll.kernel32.ZeroMemory(byref(tkprivs), sizeof(tkprivs));
-	
-	if not windll.advapi32.OpenProcessToken(
-		windll.kernel32.GetCurrentProcess(),
-		(TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY),
-		byref(hToken)):
-		raise RuntimeError("WinAPI Error: OpenProcessToken")
-		return False
-	
-	if not windll.advapi32.LookupPrivilegeValueA(NULL, lpszPriv, byref(luid)):
-		CloseHandle(hToken)
-		raise RuntimeError("WinAPI Error: LookupPrivilegeValueA")
-		return False
-	
-	tkprivs.PrivilegeCount = 1
-	tkprivs.Privileges[0].Luid = luid
-	tkprivs.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED
-	
-	bRet = windll.advapi32.AdjustTokenPrivileges(hToken, False, byref(tkprivs), sizeof(tkprivs), NULL, NULL)
-	windll.kernel32.CloseHandle(hToken)
-	return bRet
-
-EnablePriv(SE_DEBUG_NAME)
-EnablePriv(SE_INC_BASE_PRIORITY_NAME)
 
 def SimpleOpenProcess(processID):
 	hProcess = windll.kernel32.OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, False, processID )
